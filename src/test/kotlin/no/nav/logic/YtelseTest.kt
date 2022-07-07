cc10a989bfe6417a6fc391a59856471459b90362
Øydis Kind Refsum <oydis.kind.refsum@nav.no>
2022-07-07 10:08:52 +0200

    :pencil2: endret tekst i tester

--- src/test/kotlin/no/nav/logic/YtelseTest.kt
+++ src/test/kotlin/no/nav/logic/YtelseTest.kt
@@ -5,31 +5,31 @@ import io.kotest.matchers.shouldBe
 
 class YtelseTest : FunSpec({
     context("Ytelse") {
-        test("ytelse med grunnbeløp 6g") {
+        test("ytelse med grunnbeløp 6g, 0 barn og 0 arbeidsgrad") {
             val ytelse = ytelse(668862.0, 0, 0.0)
             ytelse shouldBe 441449
         }
-        test("ytelse med grunnbeløp 2g og 7 barn") {
+        test("ytelse med grunnbeløp 2g, 7 barn og 0 arbeidsgrad") {
             val ytelse = ytelse(222954.0, 7, 0.0)
             ytelse shouldBe 196290
         }
-        test("ytelse med grunnbeløp 2g og 8 barn") {
+        test("ytelse med grunnbeløp 2g, 8 barn og 0 arbeidsgrad") {
             val ytelse = ytelse(222954.0, 8, 0.0)
             ytelse shouldBe 200659
         }
-        test("ytelse med grunnbeløp 2g og 9 barn") {
+        test("ytelse med grunnbeløp 2g, 9 barn og 0 arbeidsgrad") {
             val ytelse = ytelse(222954.0, 9, 0.0)
             ytelse shouldBe 200659
         }
-        test("ytelse med grunnbeløp 6g og 22 barn") {
+        test("ytelse med grunnbeløp 6g, 22 barn og 0 arbeidsgrad") {
             val ytelse = ytelse(668862.0, 22, 0.0)
             ytelse shouldBe 595889
         }
-        test("ytelse med grunnbeløp 6g og 23 barn") {
+        test("ytelse med grunnbeløp 6g, 23 barn og 0 arbeidsgrad") {
             val ytelse = ytelse(668862.0, 23, 0.0)
             ytelse shouldBe 601976
         }
-        test("ytelse med grunnbeløp 6g og 24 barn") {
+        test("ytelse med grunnbeløp 6g, 24 barn og 0 arbeidsgrad") {
             val ytelse = ytelse(668862.0, 24, 0.0)
             ytelse shouldBe 601976
         }
